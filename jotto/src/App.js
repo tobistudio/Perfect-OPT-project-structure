f757437651d8557e4d3ef2907a7a2a18136552c6
DawnSee0823 <nazarnesterov21428@gmail.com>
2018-09-29 18:40:20 -0400

    work

---
+++ jotto/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    // };
+  }
+
+  render() {
+    return (
+      <div data-test="component-app">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ jotto/src/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if(state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+}
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without crashing', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+  // console.log(wrapper.debug());
+  // expect(wrapper).toBeFalsy();
+});
