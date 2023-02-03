61e914b97ba304b999a7e7718057bd8e5e9c5179
SamonTaikan <DawnSee0823@gmail.com>
2023-02-03 05:42:18 -0800

    feat: update reducer

--- jotto/src/reducers/successReducer.js
+++ jotto/src/reducers/successReducer.js
@@ -3,7 +3,7 @@ import { actionTypes } from "../actions";
 export default (state = false, action) => {
   switch (action.type) {
     case actionTypes.CORRECT_GUESS:
-      return true;
+      return false;
     default:
       return state;
   }
